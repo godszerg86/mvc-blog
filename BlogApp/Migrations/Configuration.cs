1a9453d5ef08a03bfe2aa6d4efd74fa7455e996a
Sergei Belialov <32887986+godszerg86@users.noreply.github.com>
2018-09-20 22:11:10 -0500

    Add admin and moderator roles to seed method

--- BlogApp/Migrations/Configuration.cs
+++ BlogApp/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace BlogApp.Migrations
 {
+    using BlogApp.Models;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using System;
@@ -22,10 +23,42 @@ namespace BlogApp.Migrations
             //  to avoid creating duplicate seed data.
 
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
             if (!context.Roles.Any(r => r.Name == "Admin"))
             {
                 roleManager.Create(new IdentityRole { Name = "Admin" });
             }
+
+            if (!context.Roles.Any(r => r.Name == "Moderator"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Moderator" });
+            }
+
+            ApplicationUser adminUser = null; // why not new () like below
+
+            if (!context.Users.Any(item => item.UserName == "admin@admin.com"))
+            {
+                adminUser = new ApplicationUser(); //questiion why?
+                adminUser.UserName = "admin@admin.com";
+                adminUser.Email = "admin@admin.com";
+                adminUser.LastName = "Admin";
+                adminUser.FirstName = "Zerg";
+                adminUser.DisplayName = "z3rg";
+                
+
+                userManager.Create(adminUser, "Password-1");
+            } else
+            {
+                adminUser = context.Users.FirstOrDefault(item => item.UserName == "admin@admin.com"); // point of else?
+            }
+
+            if (!userManager.IsInRole(adminUser.Id,"Admin"))
+            {
+                userManager.AddToRole(adminUser.Id, "Admin");
+            }
+
         }
     }
 }
